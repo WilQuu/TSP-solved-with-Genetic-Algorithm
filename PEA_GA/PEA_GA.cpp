95e1fbc0dbd238039f5a0cb50c5509dfc3d32b7f
Grzegorz Kuchta <grzes_kuchta@wp.pl>
2021-01-13 19:01:44 +0100

    base

---
+++ PEA_GA/PEA_GA.cpp
@@ -0,0 +1,130 @@
+﻿#include <iostream>
+#include <fstream>
+#include<string>
+#include<cstdlib>
+#include <sstream>
+#include <cstring>
+#include<time.h>
+#include<math.h>
+#include<ctime>
+#include<vector>
+using namespace std;
+
+void randomPath(int* path, int citiesNum) {
+    vector<int> tempNums;
+    for (int i = 1; i < citiesNum; i++)
+        tempNums.push_back(i);
+
+    path[0] = 0;
+    path[citiesNum] = 0;
+    int currentVertex;
+    vector<int> cleaningVector;
+    for (int i = 1; i < citiesNum; i++) {
+        currentVertex = rand() % tempNums.size();
+        vector<int>::iterator it;
+        it = tempNums.begin() + currentVertex;
+        path[i] = tempNums.at(currentVertex);
+        tempNums.erase(it);
+
+    }
+}
+
+void printPath(int arr[], int size) {
+
+    for (int i = 0; i < size; i++) {
+        if (i == size - 1) {
+            cout << arr[i] << endl;
+        }
+        else
+            cout << arr[i] << " -> ";
+
+    }
+}
+void copyArray(int* arrA, int* arrB, int size) {
+    for (int i = 0; i < size; i++)
+        arrA[i] = arrB[i];
+}
+
+int countCost(int* path, int citiesNum, int** cityMatrix) {
+    int cost = 0;
+    for (int i = 0; i < citiesNum; i++) {
+        cost += cityMatrix[path[i]][path[i + 1]];
+    }
+    cost += cityMatrix[path[citiesNum]][path[0]];
+    return cost;
+}
+
+int main() {
+    srand(time(NULL));
+    fstream file;
+    string tempLine;
+    int hamiltonOpt;
+    int citiesNum;
+    string fileName;
+    cout << "Name of instance : ";
+    cin >> fileName;
+    file.open(fileName, std::ios::in | std::ios::out);
+    if (file.is_open()) {
+        getline(file, tempLine);
+        getline(file, tempLine);
+        citiesNum = atoi(tempLine.c_str());
+        int** cityMatrix = new int* [citiesNum];
+        for (int i = 0; i < citiesNum; i++)
+            cityMatrix[i] = new int[citiesNum];
+
+
+        for (int i = 0; i < citiesNum; i++) {
+            getline(file, tempLine);
+            stringstream ss;
+            ss << tempLine;
+            int found;
+            string temp;
+            int y = 0;
+            while (!ss.eof()) {
+                ss >> temp;
+                if (stringstream(temp) >> found) {
+                    cityMatrix[i][y] = found;
+                    y++;
+                }
+            }
+
+            temp = "";
+        }
+        getline(file, tempLine);
+        hamiltonOpt = atoi(tempLine.c_str());
+
+        cout << "------------city matrix------------" << endl;
+
+        for (int i = 0; i < citiesNum; i++) {
+            for (int j = 0; j < citiesNum; j++)
+                cout << *(*(cityMatrix + i) + j) << " ";
+            cout << endl;
+        }
+
+        /*    deklarowanie zmiennych potrzebnych do wykonania algorytmu   */
+
+        clock_t start = clock();
+      
+        /*   tutaj zaczyna sie algorytm GA    */
+          
+
+
+        /*   tutaj konczy  sie algorytm GA      */
+
+        clock_t stop = clock();
+        
+        cout << "Time : " << stop - start << " [s]" << endl;
+
+        /*        zwalnianie pamieci               */
+        for (int i = 0; i < citiesNum; i++)
+            delete[] cityMatrix[i];
+        delete[]cityMatrix;
+
+    }
+    else cerr << "Dostep do pliku zostal zabroniony!" << endl;
+
+
+
+    file.close();
+    return 0;
+}
